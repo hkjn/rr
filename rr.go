2ba72241d0f27335ad000c3e8e44fc1baf93290f
Henrik Jonsson <me@hkjn.me>
2016-05-22 17:37:33 +0200

    Add rr.go

---
+++ rr.go
@@ -0,0 +1,8 @@
+// The rr tool reports status of repos.
+package main
+
+func main() {
+	// TODO(hkjn): Run $GOPATH/src/hkjn.me/git-wtf.rb for all
+	// directories in REPOBASE, default to $GOPATH/src/hkjn.me.
+	// TODO(hkjn): Send HTTP POST to COLLECTOR_URpL
+}
